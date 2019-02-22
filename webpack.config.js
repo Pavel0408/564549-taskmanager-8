a56ebf38474fa5f7c797b332d639796788390cad
Pavel <pavel0408@gmail.com>
2019-02-22 16:57:14 +0300

    добавил карту и сервер

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,12 @@ module.exports = {
   output: {
     filename: `bundle.js`,
     path: path.join(__dirname, `public`)
+  },
+  devtool: `source-map`,
+  devServer: {
+    contentBase: path.join(__dirname, `public`),
+    publicPath: 'http://localhost:8080/',
+    hot: true,
+    compress: false
   }
 };
