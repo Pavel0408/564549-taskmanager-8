86496abd83ef11019faaebff10814be29c39c1a0
Pavel <pavel0408@gmail.com>
2019-02-26 14:51:12 +0300

    добавил модуль с моковыми массивами

---
+++ src/mock/mock-arrays.js
@@ -0,0 +1,38 @@
+const titles = [
+  `изучить теорию`,
+  `cделать домашку`,
+  `пройти интенсив на соточку`
+];
+
+const tags = [
+  `homework`,
+  `theory`,
+  `practice`,
+  `intensive`,
+  `keks`
+];
+
+const colors = [
+  `black`,
+  `yellow`,
+  `blue`,
+  `green`,
+  `pink`
+];
+
+const repeatingDays = [
+  `Mo`,
+  `Tu`,
+  `We`,
+  `Th`,
+  `Fr`,
+  `Sa`,
+  `Su`
+];
+
+export default {
+  titles,
+  tags,
+  colors,
+  repeatingDays
+};
