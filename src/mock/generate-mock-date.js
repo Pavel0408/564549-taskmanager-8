afcd6029d3d2825cb36cf0690be5e97c30b46389
Pavel <pavel0408@gmail.com>
2019-02-28 18:34:56 +0300

    измени рссчёс мс в неделе

--- src/mock/generate-mock-date.js
+++ src/mock/generate-mock-date.js
@@ -3,7 +3,7 @@ import {
 } from "../utilities";
 
 export const gnerateMockDate = () => {
-  const MS_IN_WEEK = 7 * 24 * 60 * 60 * 1000;
+  const MS_IN_WEEK = 1000 * 60 * 60 * 24 * 7;
   const dateNow = new Date();
   return new Date(getRandomNumber(+dateNow - MS_IN_WEEK, +dateNow + MS_IN_WEEK));
 };
