aa9f5f71b00e28765d1247ff848bf9d830395509
Pavel <pavel0408@gmail.com>
2019-02-28 18:47:13 +0300

    заменил + на valueOf в преобразовании даты

--- src/mock/generate-mock-date.js
+++ src/mock/generate-mock-date.js
@@ -5,5 +5,5 @@ import {
 export const gnerateMockDate = () => {
   const MS_IN_WEEK = 1000 * 60 * 60 * 24 * 7;
   const dateNow = new Date();
-  return new Date(getRandomNumber(+dateNow - MS_IN_WEEK, +dateNow + MS_IN_WEEK));
+  return new Date(getRandomNumber(dateNow.valueOf() - MS_IN_WEEK, dateNow.valueOf() + MS_IN_WEEK));
 };
