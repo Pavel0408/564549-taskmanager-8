89bcf10c111ec724d6ff61523f484f1c4c51f238
Pavel <pavel0408@gmail.com>
2019-02-28 18:15:42 +0300

    поправил коментарии

--- src/utilities.js
+++ src/utilities.js
@@ -4,12 +4,12 @@ export const getRandomNumber = (min, max) => {
   return rand;
 };
 
-// случайный индекс массива
+// Случайный индекс массива
 export const getRandomIndex = (arr) => {
   return getRandomNumber(0, arr.length - 1);
 };
 
-// случайное значение из массива
+// Случайное значение из массива
 export const getRandomValue = function (arr) {
   const randInd = getRandomIndex(arr);
   const val = arr[randInd];
