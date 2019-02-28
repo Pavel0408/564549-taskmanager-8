71ae78f301ae3b0c1d66e6a1a23e5d3d958d1417
Pavel <pavel0408@gmail.com>
2019-02-28 18:03:56 +0300

    сделал MockCard объектом

--- src/mock/generate-mock-card.js
+++ src/mock/generate-mock-card.js
@@ -13,13 +13,15 @@ import {
 
 import mokArrays from "./mock-arrays";
 
-export const MockCard = function () {
-  this.title = getRandomValue(mokArrays.titles);
-  this.dueDate = gnerateMockDate();
-  this.tags = new Set(mokArrays.tags.slice(0, getRandomNumber(1, mokArrays.tags.length - 1)));
-  this.picture = `http://picsum.photos/100/100?r=${Math.random()}`;
-  this.color = getRandomValue(mokArrays.colors);
-  this.repeatingDays = new RepeatingDays();
-  this.isFavorite = getRandomValue(mokArrays.boolArray);
-  this.isDone = getRandomValue(mokArrays.boolArray);
+export const mockCard = function () {
+  return {
+    title: getRandomValue(mokArrays.titles),
+    dueDate: gnerateMockDate(),
+    tags: new Set(mokArrays.tags.slice(0, getRandomNumber(1, mokArrays.tags.length - 1))),
+    picture: `http://picsum.photos/100/100?r=${Math.random()}`,
+    color: getRandomValue(mokArrays.colors),
+    repeatingDays: new RepeatingDays(),
+    isFavorite: getRandomValue(mokArrays.boolArray),
+    isDone: getRandomValue(mokArrays.boolArray)
+  };
 };
--- src/mock/generate-mock-cards-array.js
+++ src/mock/generate-mock-cards-array.js
@@ -1,11 +1,11 @@
 import {
-  MockCard
+  mockCard
 } from "./generate-mock-card";
 
 export const generateCardsArray = (cardsNumber) => {
   const cardsArr = [];
   for (let i = 0; i < cardsNumber; i++) {
-    const card = new MockCard();
+    const card = mockCard();
     cardsArr.push(card);
   }
   return cardsArr;
