cca973b3e18f3785d29e87d7a5e2199bfe9f2af2
Pavel <pavel0408@gmail.com>
2019-03-01 14:13:53 +0300

    vlueOf -> getTime

--- src/mock/generate-mock-date.js
+++ src/mock/generate-mock-date.js
@@ -5,5 +5,5 @@ import {
 export const gnerateMockDate = () => {
   const MS_IN_WEEK = 1000 * 60 * 60 * 24 * 7;
   const dateNow = new Date();
-  return new Date(getRandomNumber(dateNow.valueOf() - MS_IN_WEEK, dateNow.valueOf() + MS_IN_WEEK));
+  return new Date(getRandomNumber(dateNow.getTime() - MS_IN_WEEK, dateNow.getTime() + MS_IN_WEEK));
 };
